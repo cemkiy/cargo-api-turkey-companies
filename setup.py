5b91a5d1c3867f3c32a30589ba038465eae6f603
cemkiy <se.cemkiy@gmail.com>
2014-12-05 15:52:44 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='cargo_api',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='GNU V3',
+    author='cemkiy',
+    author_email='se.cemkiy@gmail.com',
+    description='Cargo Checked Applicaiton by cargo no.yurtiçi kargo,aras kargo,mng kargo'
+)
